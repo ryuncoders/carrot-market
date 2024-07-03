26cac7e4dfca21ea29bf01849636ec1f7a02a7ab
ryuncoders <seungjy10@gmail.com>
2024-07-03 15:04:41 +0900

    Tailwind(Lists, Animations): 짝수(odd:) .. , 마지막(last:), 비어있는 리스트(empty:), 애니메이션(animate-ping, ...)

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,31 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="bg-gray-600 h-screen flex items-center justify-center p-5">
-      <form
-        className="bg-white p-3 rounded-2xl max-w-screen-lg 
-      ring ring-transparent transition-shadow has-[:invalid]:ring-red-100 has-[.peer:focus]:bg-cyan-600"
-      >
-        <div
-          className="my-3 flex flex-col gap-2 *:outline-none 
-         "
-        >
-          <input
-            type="email"
-            placeholder="email"
-            required
-            className="px-5 py-2 bg-slate-200 rounded-full w-full 
-            ring ring-transparent focus:ring-green-500 
-            focus:ring-offset-2 transition-shadow
-             invalid:focus:ring-red-600 peer"
-          />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="password"
-            minLength={5}
-            required
-            className="px-5 py-2 bg-slate-200 rounded-full w-full 
-            ring ring-transparent ring-offset-2 transition-shadow
-             focus:ring-green-500 invalid:focus:ring-red-500
-            
-            "
-          />
-          <span className=" text-red-500 font-medium hidden peer-invalid:block ">
-            Email is required.
-          </span>
-        </div>
+      <div className="max-w-screen-md min-w-40 flex justify-center items-center p-5 rounded-2xl bg-white">
+        <ul>
+          {["Jane Cooper", "Leslie Alexander", "Elannor Pona", ""].map(
+            (item, index) => (
+              <li
+                key={index}
+                className="flex items-center gap-2 mb-3 last:mb-0 *:flex *:justify-center *:items-center"
+              >
+                <div className="size-8 bg-blue-400 rounded-full " />
+                <span className="flex items-center font-bold empty:bg-gray-300 empty:h-3 empty:w-20 empty:animate-pulse">
+                  {item}
+                </span>
+                <div className="bg-red-500 text-white rounded-full size-5 text-xs relative z-10">
+                  <span className="z-10">{index}</span>
+                  <div className="bg-red-500 rounded-full size-5 absolute animate-ping" />
+                </div>
 
-        <button
-          type="submit"
-          className="bg-black text-white w-full rounded-full py-2 font-bold"
-        >
-          Log in
-        </button>
-      </form>
+                <div className="ml-auto bg-gray-300 size-8 rounded-full font-semibold">
+                  {">"}
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
     </main>
   );
 }
