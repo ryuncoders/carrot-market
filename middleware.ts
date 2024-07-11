dcab874aaf9304ee51debd9d15b9515236829fa4
ryuncoders <seungjy10@gmail.com>
2024-07-11 21:24:31 +0900

    Middleware: Authenticaion

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,31 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/create-account": true,
+  "/sms": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("hello middleware");
+  const session = await getSession();
+  const exits = publicOnlyUrls[request.nextUrl.pathname];
+  // session 없는 경우
+  if (!session.id) {
+    // 접근가능한 주소가 아닌경우
+    if (!exits) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exits) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
