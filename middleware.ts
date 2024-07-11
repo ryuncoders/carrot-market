71ecdf6672962f1cc115891b8325e9a2965351e2
ryuncoders <seungjy10@gmail.com>
2024-07-11 21:02:26 +0900

    Middleware(config): edge runtime(특정페이지에서만 실행되게 설정)

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
-import { redirect } from "next/navigation";
-import { url } from "inspector";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log("session", session);
-  console.log("request.url", request.url);
-
-  return NextResponse.redirect(new URL("/", request.url));
+  console.log("hello middleware");
 }
+
+export const config = {
+  //   matcher: ["/", "/profile", "/create-account"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
