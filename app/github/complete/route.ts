05f82a78fc0533e6ac33475c5ef41c199c4215ee
ryuncoders <seungjy10@gmail.com>
2024-07-12 12:04:23 +0900

    Social authentication(github): github가 사용자를 사이트 다시 리디렉션

---
+++ app/github/complete/route.ts
@@ -0,0 +1,23 @@
+import { notFound } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get("code");
+  if (!code) {
+    return notFound();
+  }
+  const accessTokenParams = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    client_secret: process.env.GITHUB_CLIENT_SECRET!,
+    code,
+  });
+  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+  const accessTokenResponse = await fetch(accessTokenURL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const accessTokenData = await accessTokenResponse.json();
+  return Response.json({ accessTokenData });
+}
