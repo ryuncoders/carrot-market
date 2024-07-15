f9a2779863a859c08090e83fbe08488e89aa6371
ryuncoders <seungjy10@gmail.com>
2024-07-15 11:19:39 +0900

    refactor

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,4 +1,5 @@
 import db from "@/lib/db";
+import LoginSession from "@/lib/login-session";
 import getSession from "@/lib/session";
 import { notFound, redirect } from "next/navigation";
 import { NextRequest } from "next/server";
@@ -33,33 +34,31 @@ export async function GET(request: NextRequest) {
     },
     cache: "no-cache",
   });
+
   const { id, avatar_url, login } = await userProfileResponse.json();
+
   const user = await db.user.findUnique({
     where: {
-      github_id: id,
+      github_id: id + "",
     },
     select: {
       id: true,
     },
   });
   if (user) {
-    const session = await getSession();
-    session.id = user.id;
-    await session.save();
+    await LoginSession(user);
     return redirect("/profile");
   }
   const newUser = await db.user.create({
     data: {
       username: `${login}_gh`,
-      github_id: id,
+      github_id: id + "",
       avatar: avatar_url,
     },
     select: {
       id: true,
     },
   });
-  const session = await getSession();
-  session.id = newUser.id;
-  await session.save();
+  await LoginSession(newUser);
   return redirect("/profile");
 }
