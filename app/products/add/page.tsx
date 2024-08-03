822e4075b0db30489fac574e45d160e080be453b
ryuncoders <seungjy10@gmail.com>
2024-08-03 15:56:17 +0900

    Product Upload: add

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,9 +5,11 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { files },
@@ -16,12 +18,24 @@ export default function AddProduct() {
       return;
     }
     const file = files[0];
+    const fileType = file.type;
+    if (!fileType.startsWith("image/")) {
+      setErrorMessage("사진이 아닙니다. 다시 선택해주세요.");
+      return;
+    }
+    const fileSize = file.size;
+    const MB = 1024 * 1024;
+    if (fileSize > MB * 3) {
+      setErrorMessage("사진의 크기가 3MB이하여야 합니다.");
+      return;
+    }
     const url = URL.createObjectURL(file);
     setPreview(url);
   };
+  const [state, action] = useFormState(uploadProduct, null);
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="bg-center bg-cover border-2 aspect-square flex justify-center items-center flex-col border-dashed cursor-cursor-pointer rounded-md"
@@ -34,6 +48,7 @@ export default function AddProduct() {
               <PhotoIcon className="w-20" />
               <div className="text-neutral-400 text-sm">
                 사진을 추가해주세요.
+                {state?.fieldErrors.photo}
               </div>
             </>
           ) : null}
@@ -46,9 +61,23 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" required placeholder="가격" type="number" />
+        <span>{errorMessage}</span>
         <Input
+          name="title"
+          errors={state?.fieldErrors.title}
+          required
+          placeholder="제목"
+          type="text"
+        />
+        <Input
+          name="price"
+          errors={state?.fieldErrors.price}
+          required
+          placeholder="가격"
+          type="number"
+        />
+        <Input
+          errors={state?.fieldErrors.description}
           name="description"
           required
           placeholder="자세한 설명"
