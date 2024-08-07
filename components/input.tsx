3f0adb24fea72494a4f1e89e375c195f65ae9ab6
ryuncoders <seungjy10@gmail.com>
2024-08-07 11:28:50 +0900

    Fixes: #1: Add forwardRef

--- components/input.tsx
+++ components/input.tsx
@@ -1,27 +1,35 @@
-import { InputHTMLAttributes } from "react";
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from "react";
 
 interface InputProps {
   errors?: string[];
   name: string;
 }
 
-export default function Input({
-  errors = [],
-  name,
-  ...rest
-}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+const _Input = (
+  {
+    errors = [],
+    name,
+    ...rest
+  }: InputProps & InputHTMLAttributes<HTMLInputElement>,
+  ref: ForwardedRef<HTMLInputElement>
+) => {
   return (
     <div className="flex flex-col gap-2">
       <input
+        ref={ref}
         {...rest}
         name={name}
         className=" bg-transparent focus:outline-none ring-2 ring-neutral-200  
         border-none placeholder:text-neutral-400 rounded-md w-full 
         focus:ring-main-color focus:ring-4 transition "
       />
-      {errors.map((error) => (
-        <span className="text-red-600 font-normal">{error}</span>
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-600 font-normal">
+          {error}
+        </span>
       ))}
     </div>
   );
-}
+};
+
+export default forwardRef(_Input);
