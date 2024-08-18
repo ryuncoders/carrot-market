a29e55a9a07cdff1c62db1f1de698cfff2b24fdb
ryuncoders <seungjy10@gmail.com>
2024-08-18 13:03:04 +0900

    Refactor-code

--- app/(auth)/github/complete/route.ts
+++ app/(auth)/github/complete/route.ts
@@ -1,4 +1,3 @@
-import UpdateUsername from "@/app/(auth)/update/username/page";
 import db from "@/lib/db";
 import { getAccessToken } from "@/lib/github/getAccessToken";
 import { getUserEmail } from "@/lib/github/getUserEmail";
