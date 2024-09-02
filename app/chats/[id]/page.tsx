83c3cdf3d5f020f1de0373ffa302bc5612918e79
ryuncoders <seungjy10@gmail.com>
2024-09-02 12:20:58 +0900

    Chat Page: use supabase

---
+++ app/chats/[id]/page.tsx
@@ -0,0 +1,69 @@
+import ChatMessagesList from "@/components/chat-messages-list";
+import db from "@/lib/db";
+import getSession from "@/lib/session/get";
+import { Prisma } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+async function getRoom(id: string) {
+  const room = await db.chatRoom.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      users: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+  if (room) {
+    const session = await getSession();
+    const canSee = room.users.find((user) => user.id === session.id!);
+    if (!canSee) {
+      return notFound;
+    }
+  }
+  return room;
+}
+
+async function getMessages(chatRoomId: string) {
+  const messages = db.message.findMany({
+    where: {
+      chatRoomId,
+    },
+    select: {
+      id: true,
+      payload: true,
+      created_at: true,
+      userId: true,
+      user: {
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+  });
+  return messages;
+}
+
+export type InitialChatMessages = Prisma.PromiseReturnType<typeof getMessages>;
+
+export default async function Chat({ params }: { params: { id: string } }) {
+  const room = await getRoom(params.id);
+  if (!room) {
+    return notFound();
+  }
+  const initialMessages = await getMessages(params.id);
+  const session = await getSession();
+  return (
+    <div>
+      <ChatMessagesList
+        chatRoomId={params.id}
+        initialMessages={initialMessages}
+        userId={session.id!}
+      />
+    </div>
+  );
+}
---
+++ components/chat-messages-list.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { InitialChatMessages } from "@/app/chats/[id]/page";
+import { formatToTime } from "@/lib/utils";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import { createClient, RealtimeChannel } from "@supabase/supabase-js";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+
+interface chatMessageListProps {
+  initialMessages: InitialChatMessages;
+  userId: number;
+  chatRoomId: string;
+}
+
+const SUPABASE_URL = "https://zudjduiyvgrhmcggfsqv.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1ZGpkdWl5dmdyaG1jZ2dmc3F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyNDQzMDIsImV4cCI6MjA0MDgyMDMwMn0.zkhWYJMDWYGm8nZTOZF26ftxiKdsIZHo8Ap40aEM_f0";
+
+export default function ChatMessagesList({
+  chatRoomId,
+  initialMessages,
+  userId,
+}: chatMessageListProps) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [message, setMessage] = useState("");
+  const channel = useRef<RealtimeChannel>();
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setMessage(value);
+  };
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setMessages((prevMsgs) => [
+      ...prevMsgs,
+      {
+        id: Date.now(),
+        payload: message,
+        created_at: new Date(),
+        userId,
+        user: {
+          username: "user",
+          avatar: "xxx",
+        },
+      },
+    ]);
+    channel.current?.send({
+      type: "broadcast",
+      event: "message",
+      payload: { message },
+    });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    const client = createClient(SUPABASE_URL, SUPABASE_KEY);
+    channel.current = client.channel(`room-${chatRoomId}`);
+    channel.current
+      .on("broadcast", { event: "message" }, (payload) => {
+        console.log(payload);
+      })
+      .subscribe();
+    return () => {
+      channel.current?.unsubscribe();
+    };
+  }, [chatRoomId]);
+
+  return (
+    <div className="p-5 flex flex-col gap-3 min-h-screen justify-end">
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={`flex gap-2 items-start ${
+            message.userId === userId ? "justify-end" : ""
+          }`}
+        >
+          {message.userId === userId ? null : (
+            <Image
+              src={message.user.avatar!}
+              alt={message.user.username}
+              width={50}
+              height={50}
+              className="size-8 rounded-full"
+            />
+          )}
+          <div
+            className={`flex gap-3 items-end ${
+              message.userId === userId ? "items-end" : ""
+            }`}
+          >
+            <span
+              className={`${
+                message.userId === userId
+                  ? "bg-orange-500 order-2"
+                  : "bg-neutral-300 text-black"
+              } p-2.5 rounded-2xl`}
+            >
+              {message.payload}
+            </span>
+            <span className="text-xs text-neutral-400">
+              {formatToTime(message.created_at)}
+            </span>
+          </div>
+        </div>
+      ))}
+      <form className="flex relative" onSubmit={onSubmit}>
+        <input
+          required
+          onChange={onChange}
+          value={message}
+          className="bg-transparent rounded-full w-full h-10 focus:outline-none px-5 ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-neutral-50 border-none placeholder:text-neutral-400"
+          type="text"
+          name="message"
+          placeholder="Write a message..."
+        />
+        <button className="absolute right-0">
+          <ArrowUpCircleIcon className="size-10 text-orange-500 transition-colors hover:text-orange-300" />
+        </button>
+      </form>
+    </div>
+  );
+}
