b34b12470f8cdb30ac2d5a798dbbfac9f114f9e1
ryuncoders <seungjy10@gmail.com>
2024-09-06 17:41:22 +0900

    Extras(all segments): 파일이름

--- app/extras/[...id]/page.tsx
+++ app/extras/[...id]/page.tsx
@@ -1,4 +1,7 @@
-export default function Extras() {
+export default function Extras({ params }: { params: { id: string[] } }) {
+  console.log("params", params);
+  // id: ['1', '2', '3']
+
   return (
     <div className="flex flex-col gap-3 py-10 px-5">
       <h1 className="text-6xl font-protest">Extras!</h1>
