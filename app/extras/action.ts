b13303fc20bc5425863dc5c23d6721d6931d177a
ryuncoders <seungjy10@gmail.com>
2024-09-06 18:30:10 +0900

    Extras(이미지 최적화): public, server

--- app/extras/action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import "server-only";
-
-// client에서 fetch하면 안되는 경우
-export function fetchFromAPI() {
-  fetch(".....");
-}
--- components/hacked.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { fetchFromAPI } from "@/app/extras/action";
-
-export default function HackedComponent({}: any) {
-  fetchFromAPI();
-  return <div>hacked</div>;
-}
